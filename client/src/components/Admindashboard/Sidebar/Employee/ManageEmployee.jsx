b31b14e6099d3f92ab82c5d77fce2b4442d5c2eb
dyanix <dyanixdhawale@gmail.com>
2023-04-19 23:36:53 +0530

    added css

--- client/src/components/Admindashboard/Sidebar/Employee/ManageEmployee.jsx
+++ client/src/components/Admindashboard/Sidebar/Employee/ManageEmployee.jsx
@@ -97,14 +97,9 @@ const ManageEmployee = () => {
               <td>{employee.designation}</td>
               <td>{employee.email}</td>
               <td>
-                <button
-                  style={{ marginRight: "10px", color: "#6a14a3" }}
-                  onClick={() => handleViewClick(employee)}
-                >
-                  <AiOutlineEye />
-                </button>
-                <button style={{ marginRight: "10px", color: "#6a14a3" }} onClick={() => handleEditClick(employee)}><BsPencil /></button>
-                <button style={{ marginRight: "10px", color: "#6a14a3" }} onClick={() => handleDelete(employee._id)}><RiDeleteBinLine /></button>
+                <button style={{ marginRight: "10px", color: "#e63900", backgroundColor: "white", border: '2px solid #ccc', borderRadius: "10px" }} onClick={() => handleViewClick(employee)} >  <AiOutlineEye /> </button>
+                <button style={{ marginRight: "10px", color: "#e63900", backgroundColor: "white", border: '2px solid #ccc', borderRadius: "10px" }} onClick={() => handleEditClick(employee)}><BsPencil /></button>
+                <button style={{ marginRight: "10px", color: "#e63900", backgroundColor: "white", border: '2px solid #ccc', borderRadius: "10px" }} onClick={() => handleDelete(employee._id)}><RiDeleteBinLine /></button>
               </td>
             </tr>
           ))}
