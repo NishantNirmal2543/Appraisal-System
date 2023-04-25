cb11495e9e02c6c25908987ecdf52a425272e657
dyanix <dyanixdhawale@gmail.com>
2023-04-25 17:03:36 +0530

    added skeleton loader

--- client/src/components/Employeedashboard/Sidebar/Dashboard/Dashboard.jsx
+++ client/src/components/Employeedashboard/Sidebar/Dashboard/Dashboard.jsx
@@ -1,3 +1,58 @@
+// import React, { useState, useEffect } from "react";
+// import axios from "axios";
+// import "./Dashboard.css";
+
+// const Dashboard = () => {
+//   const [employee, setEmployee] = useState(null);
+//   const [error, setError] = useState(null);
+//   useEffect(() => {
+//     const fetchEmployee = async () => {
+//       try {
+//         const token = localStorage.getItem('token');
+//         console.log(token);
+//         const response = await axios.get("http://localhost:8080/validuser", {
+
+//           headers: {
+//             Authorization: `Bearer ${token}`,
+//           },
+//         });
+
+//         setEmployee(response.data.employee);
+//       } catch (error) {
+//         setError(error.response.data.message);
+//       }
+//     };
+//     fetchEmployee();
+//   }, []);
+
+//   return (
+//     <>
+//       <h1>Dashboard</h1>
+//       <div className="dashboard">
+//         {error && <div className="error">{error}</div>}
+//         {employee && (
+//           <div className="card">
+//             <p>Name: {employee.name}</p>
+//             <p>College: {employee.college}</p>
+
+//             <p>Designation: {employee.designation}</p>
+//             <p>Mobile: {employee.mobile}</p>
+
+//             <p>Email: {employee.email}</p>
+//             <p>Department: {employee.department}</p>
+
+//           </div>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Dashboard;
+
+
+
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./Dashboard.css";
@@ -5,19 +60,20 @@ import "./Dashboard.css";
 const Dashboard = () => {
   const [employee, setEmployee] = useState(null);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  
   useEffect(() => {
     const fetchEmployee = async () => {
       try {
         const token = localStorage.getItem('token');
         console.log(token);
         const response = await axios.get("http://localhost:8080/validuser", {
-
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
-
         setEmployee(response.data.employee);
+        setIsLoading(false);
       } catch (error) {
         setError(error.response.data.message);
       }
@@ -30,17 +86,24 @@ const Dashboard = () => {
       <h1>Dashboard</h1>
       <div className="dashboard">
         {error && <div className="error">{error}</div>}
-        {employee && (
+        {isLoading ? (
+          <div className="container">
+          <div className="card ">
+            <div className="card-img skeleton"></div>
+            <div className="card-body">
+              <h2 className="card-title skeleton"></h2>
+              <p className="card-intro skeleton"></p>
+            </div>
+          </div>
+          </div>
+        ) : (
           <div className="card">
             <p>Name: {employee.name}</p>
             <p>College: {employee.college}</p>
-
             <p>Designation: {employee.designation}</p>
             <p>Mobile: {employee.mobile}</p>
-
             <p>Email: {employee.email}</p>
             <p>Department: {employee.department}</p>
-
           </div>
         )}
       </div>
