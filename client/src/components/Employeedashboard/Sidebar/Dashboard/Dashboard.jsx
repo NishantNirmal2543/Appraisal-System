9d8ce4e9d082660fea82105e222ab266d59332ea
dyanix <dyanixdhawale@gmail.com>
2023-05-08 17:15:11 +0530

    css

--- client/src/components/Employeedashboard/Sidebar/Dashboard/Dashboard.jsx
+++ client/src/components/Employeedashboard/Sidebar/Dashboard/Dashboard.jsx
@@ -1,18 +1,20 @@
 
+
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./Dashboard.css";
+import "./Dashboard1.css";
+import profilePhoto from "../Dashboard/1630354322427.jpeg"
+import coverPhoto from "../Dashboard/images.jpeg"
 
 const Dashboard = () => {
   const [employee, setEmployee] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  
+
   useEffect(() => {
     const fetchEmployee = async () => {
       try {
         const token = localStorage.getItem('token');
-        console.log(token);
         const response = await axios.get("http://localhost:8080/validuser", {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -28,37 +30,49 @@ const Dashboard = () => {
   }, []);
 
   return (
-    <>
-     
-      <div className="dashboard">
-        {error && <div className="error">{error}</div>}
-        {isLoading ? (
-          <div className="container">
-          <div className="card ">
-            <div className="card-img skeleton"></div>
-            <div className="card-body">
-              <h2 className="card-title skeleton"> </h2>
-              <p className="card-intro skeleton"></p>
+    <div className="dashboard">
+      {error && <div className="error">{error}</div>}
+      {isLoading ? (
+        <div className="loader"></div>
+      ) : (
+        <div className="container">
+          <div className="profile">
+            <div className="cover-photo">
+              <img src={coverPhoto} alt="Cover" />
+            </div>
+            <div className="profile-details">
+              <div className="profile-photo">
+                <img src={profilePhoto} alt="Profile" />
+              </div>
+              <h1>{employee.name}</h1>
+              <h3>{employee.designation}</h3>
+              <hr />
+              <div className="info">
+                <div className="info-item">
+                  <h4>College</h4>
+                  <p>{employee.college}</p>
+                </div>
+                <div className="info-item">
+                  <h4>Department</h4>
+                  <p>{employee.department}</p>
+                </div>
+                <div className="info-item">
+                  <h4>Email</h4>
+                  <p>{employee.email}</p>
+                </div>
+                <div className="info-item">
+                  <h4>Mobile</h4>
+                  <p>{employee.mobile}</p>
+                </div>
+              </div>
             </div>
           </div>
-          </div>
-        ) : (
-         
-          <div className="container">
-          <div className="section">
-            <p>Name: {employee.name}</p>
-            <p>College: {employee.college}</p>
-            <p>Designation: {employee.designation}</p>
-            <p>Mobile: {employee.mobile}</p>
-            <p>Email: {employee.email}</p>
-            <p>Department: {employee.department}</p>
-          </div>
-          </div>
-        
-        )}
-      </div>
-    </>
+        </div>
+      )}
+    </div>
   );
 };
 
 export default Dashboard;
+
+
