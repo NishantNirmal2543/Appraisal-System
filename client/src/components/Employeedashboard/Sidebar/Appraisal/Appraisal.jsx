e75d9f86a60f2df0d99b1082ecc56df533d4cd76
dyanix <dyanixdhawale@gmail.com>
2023-05-11 23:57:26 +0530

    c

--- client/src/components/Employeedashboard/Sidebar/Appraisal/Appraisal.jsx
+++ client/src/components/Employeedashboard/Sidebar/Appraisal/Appraisal.jsx
@@ -1004,12 +1004,8 @@ const Appraisal = () => {
                     <h2 style={{ textAlign: "center" }}>PART-A : Teaching Learning performance</h2>
                     <label className="department-label">
                         <h3>Year of Performance Appraisal:</h3>
-                        <select className="department-select" value={year} onChange={handleYearChange}>
-                            <option value="">Select Year</option>
-                            <option value="2023">2023</option>
-                            <option value="2024">2024</option>
-                            <option value="2025">2025</option>
-                        </select>
+                        <input type="number" value={year} onChange={handleYearChange}>        
+                        </input>
                     </label>
                     <br />
 
