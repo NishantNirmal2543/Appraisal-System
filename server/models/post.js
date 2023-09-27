545ce9f2074502d24fcf45944eb64d0dcbaa18d3
dyanix <dyanixdhawale@gmail.com>
2023-09-27 19:39:11 +0530

    posts

---
+++ server/models/post.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+// Define a schema for your data
+const documentSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  employeeid: {
+    type:  String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  
+  description: {
+    type: String,
+    required: true,
+  },
+  picturePath: {
+    type: String,
+    required: true,
+  },
+  userPicturePath: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Object,
+    default: {},
+  },
+});
+
+// Create a MongoDB model using the schema
+const Document = mongoose.model('Document', documentSchema);
+
+module.exports = Document;
