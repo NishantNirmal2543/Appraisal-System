f1f4d04ee986b2a766db004b746e46be8302f41a
dyanix <dyanixdhawale@gmail.com>
2024-02-06 23:02:47 +0530

    chart

--- client/src/components/Employeedashboard/Sidebar/Feedback/Feedback.jsx
+++ client/src/components/Employeedashboard/Sidebar/Feedback/Feedback.jsx
@@ -1,13 +1,32 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import { Doughnut } from 'react-chartjs-2';
+import './Feedback.css'; 
 const Feedback = () => {
- 
-
-  
+  // Dummy data
+  const feedbackData = {
+    labels: ['Positive', 'Improvement', 'Appreciation'],
+    datasets: [
+      {
+        data: [20, 30, 50],
+        backgroundColor: ['#14F33D', '#F38714', '#1468F3'],
+      },
+    ],
+  };
 
   return (
     <>
-   
+    <div className='chart'>
+
+      <div className='first'>
+        <h2>Feedback Received</h2>
+        <Doughnut data={feedbackData} />
+      </div>
+
+      <div className='second'>
+        <h2>Feedback Submitted</h2>
+        <Doughnut data={feedbackData} />
+      </div>
+    </div>
     </>
   );
 };
